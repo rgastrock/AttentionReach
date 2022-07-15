b8e552b4642db9a7aae0b2ca81e5f73fa536a5ce
Raphael Gastrock <raphaelgastrock@gmail.com>
2022-07-15 11:55:07 +0200

    corrected RT and velocity plots

--- R/RT.R
+++ R/RT.R
@@ -1,7 +1,9 @@
 source('R/shared.R')
+
+#uncorrected----
 #plot RT across blocks, separate target only (TO) and target + distractor (TD) trials
 # blocks 0, 6, 12, 18 are for presses
-getTrialRT <- function(id, response, block){
+getTrialuncRT <- function(id, response, block){
   #read in data (press): one participant for now
   data <- read.csv(file=sprintf('data/p0%2s/p0%2s-%s-b%2s.csv', id, id, response, block))
   
@@ -41,7 +43,7 @@ getTrialRT <- function(id, response, block){
 }
 
 #for this one person, we could probably plot TO and TD means per block
-getTargetOnlyRTCI <- function(id = '00', response = 'press', blocks = c('00', '06', '12', '18')){
+getTargetOnlyuncRTCI <- function(id = '00', response = 'press', blocks = c('00', '06', '12', '18')){
   
   targetCI <- data.frame()
   
@@ -58,13 +60,182 @@ getTargetOnlyRTCI <- function(id = '00', response = 'press', blocks = c('00', '0
   return(targetCI)
 }
 
-getTargetDistRTCI <- function(id = '00', response = 'press', blocks = c('00', '06', '12', '18')){
+getTargetDistuncRTCI <- function(id = '00', response = 'press', blocks = c('00', '06', '12', '18')){
   
   targdistCI <- data.frame()
   
   for(block in blocks){
     dat <- getTrialRT(id = id, response = response, block = block)
     td <- dat[which(dat$condition == 'td'),]
+    td_CI <- getConfidenceInterval(td$responsetime)
+    td_CI <- data.frame(t(td_CI))
+    
+    
+    targdistCI <- rbind(targdistCI, td_CI)
+    
+  }
+  return(targdistCI)
+}
+
+plotuncRT <- function(trialtype = c('td', 'to'), target='inline'){
+  
+  #but we can save plot as svg file
+  if (target=='svg'){
+    svglite(file='doc/fig/Fig1_UncorrectedKeyPressRT.svg', width=10, height=7, pointsize=14, system_fonts=list(sans="Arial"))
+  }
+  
+  plot(NA, NA, xlim = c(0,5), ylim = c(350,750), 
+       xlab = "block", ylab = "reaction time (ms)", frame.plot = FALSE, #frame.plot takes away borders
+       main = "Key press trials", xaxt = 'n', yaxt = 'n')
+  
+  axis(side=1, at=c(1, 2, 3, 4))
+  axis(2, at = c(400, 420, 440, 460, 480, 500, 520, 540, 560, 580, 600, 620, 640, 660, 680, 700), las = 2)
+  
+  
+  
+  for (type in trialtype) {
+    blockno <- 0
+    
+    if(type == 'to'){
+      blocked <- getTargetOnlyuncRTCI()
+    } else {
+      blocked <- getTargetDistuncRTCI()
+    }
+    
+    colourscheme <- getColourScheme(trialtype=type)
+    #get bootstrapped 2.5, 50, 97.5% CIs of percentages
+    #meandist <- getConfidenceInterval(data=blocked, method='b')
+    #meandist <- getAngularReachDevsCI(data = blocked, group = group)
+    
+    for(blk in 1:nrow(blocked)){
+      if(type == 'to'){
+        blockno <- (blockno + 1) - 0.10 #counter for block, so that we can refer to it in x coordinates
+        subdat <- blocked[blk,]
+        col <- colourscheme[[type]][['S']]
+        lines(x=rep(blockno,2),y=c(subdat[[1]], subdat[[3]]),col=col) #lower and upper CI
+        points(x=blockno,y=subdat[[2]],pch=16,cex=1.5,col=col) #50% plotted as a point
+        blockno <- blockno + 0.10
+      } else {
+        blockno <- (blockno + 1) + 0.10 #counter for block, so that we can refer to it in x coordinates
+        subdat <- blocked[blk,]
+        col <- colourscheme[[type]][['S']]
+        lines(x=rep(blockno,2),y=c(subdat[[1]], subdat[[3]]),col=col) #lower and upper CI
+        points(x=blockno,y=subdat[[2]],pch=16,cex=1.5,col=col) #50% plotted as a point
+        blockno <- blockno - 0.10
+      }
+
+    }
+    
+    
+  }
+  
+  #add legend
+  legend(3,700,legend=c('target only','target + distractor'),
+         col=c(colourscheme[['to']][['S']],colourscheme[['td']][['S']]),
+         lty=1,bty='n',cex=1,lwd=2)
+  
+  #close everything if you saved plot as svg
+  if (target=='svg') {
+    dev.off()
+  }
+  
+}
+
+#corrected----
+#account for errors: remove wrong responses
+#remove trials faster or slower than 2 or 3 stdev in each block (block mean and stdev are considered for each block)
+
+getTrialRT <- function(id, response, block){
+  #read in data (press): one participant for now
+  data <- read.csv(file=sprintf('data/p0%2s/p0%2s-%s-b%2s.csv', id, id, response, block))
+  
+  #for every trial, determine RT, determine if TO or TD
+  trials <- unique(data$trial)
+  
+  responsetime <- c()
+  condition <- c()
+  trial <- c()
+  trial_error <- c()
+  
+  for(trialno in trials){
+    
+    subdat <- data[which(data$trial == trialno),]
+    error <- unique(subdat$trial_error)[2] #first will always be NA
+    distangle <- unique(subdat$distractorangle_deg)
+    if(is.na(distangle)){
+      cond <- 'to'
+    } else{
+      cond <- 'td'
+    }
+    
+    gosig <- subdat[which(subdat$step == 2),]
+    gotime <- gosig$time_ms[length(gosig$time_ms)]
+    
+    keysig <- subdat[which(subdat$step == 4),]
+    presstime <- keysig$time_ms[length(keysig$time_ms)]
+    
+    rt = presstime - gotime
+    
+    trialno <- trialno + 1 #account for python starting from 0
+    trial <- c(trial, trialno)
+    responsetime <- c(responsetime, rt)
+    condition <- c(condition, cond)
+    trial_error <- c(trial_error, error)
+  }
+  
+  df <- data.frame(trial, responsetime, condition, trial_error)
+  return(df)
+}
+
+getBlockRT <- function(id = '00', response = 'press', blocks = c('00', '06', '12', '18')){
+  
+  blockdat <- data.frame()
+  
+  for(block in blocks){
+    dat <- getTrialRT(id = id, response = response, block = block)
+    #remove trial error = 1 (trials with errors)
+    ndat <- dat[-which(dat$trial_error == 1),]
+
+    blockmu <- mean(ndat$responsetime, na.rm = T)
+    blocksigma <- sd(ndat$responsetime, na.rm = T)
+    blockclip <- abs(blockmu) + (blocksigma * 2)
+    
+    ndat$responsetime[which(abs(ndat$responsetime) > blockclip)] <- NA
+    ndat$blockno <- rep(block, nrow(ndat))
+    blockdat<- rbind(blockdat, ndat)
+    
+  }
+  return(blockdat)
+}
+
+getTargetOnlyRTCI <- function(blocks = c('00', '06', '12', '18')){
+  
+  dat <- getBlockRT()
+  targetCI <- data.frame()
+  
+  for(block in blocks){
+    subdat <- dat[which(dat$blockno == block),]
+    to <- subdat[which(subdat$condition == 'to'),]
+    to <- na.omit(to)
+    to_CI <- getConfidenceInterval(to$responsetime)
+    to_CI <- data.frame(t(to_CI))
+    
+    
+    targetCI <- rbind(targetCI, to_CI)
+    
+  }
+  return(targetCI)
+}
+
+getTargetDistRTCI <- function(blocks = c('00', '06', '12', '18')){
+  
+  dat <- getBlockRT()
+  targdistCI <- data.frame()
+  
+  for(block in blocks){
+    subdat <- dat[which(dat$blockno == block),]
+    td <- subdat[which(subdat$condition == 'td'),]
+    td <- na.omit(td)
     td_CI <- getConfidenceInterval(td$responsetime)
     td_CI <- data.frame(t(td_CI))
     
@@ -121,7 +292,7 @@ plotRT <- function(trialtype = c('td', 'to'), target='inline'){
         points(x=blockno,y=subdat[[2]],pch=16,cex=1.5,col=col) #50% plotted as a point
         blockno <- blockno - 0.10
       }
-
+      
     }
     
     
@@ -138,3 +309,26 @@ plotRT <- function(trialtype = c('td', 'to'), target='inline'){
   }
   
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- R/velocityplots.R
+++ R/velocityplots.R
@@ -1,23 +1,104 @@
-data <- read.csv(file='data/p001-reach-b00.csv')
+# data <- read.csv(file='data/p001-reach-b00.csv')
+# 
+# trials <- unique(data$trial)
+# 
+# for(trialno in trials){
+#   
+#   subdat <- data[which(data$trial == trialno),]
+#   subdat_step1 <- subdat[which(subdat$step == 1),]
+#   subdat_step2 <- subdat[which(subdat$step == 2),]
+#   subdat_step3 <- subdat[which(subdat$step == 3),]
+#   subdat_step4 <- subdat[which(subdat$step == 4),]
+#   
+#   subdat_steps <- rbind(subdat_step1, subdat_step2, subdat_step3, subdat_step4)
+#   
+#   x <- subdat_steps$mousex_px
+#   y <- subdat_steps$mousey_px
+#   t <- subdat_steps$time_ms
+#   
+#   ab <- sqrt(diff(x)^2 + diff(y)^2) / diff(t)
+#   
+# }
+# 
+# sqrt(diff(x)^2 + diff(y)^2) / diff(t)
 
-trials <- unique(data$trial)
 
-for(trialno in trials){
-  
-  subdat <- data[which(data$trial == trialno),]
-  subdat_step1 <- subdat[which(subdat$step == 1),]
-  subdat_step2 <- subdat[which(subdat$step == 2),]
-  subdat_step3 <- subdat[which(subdat$step == 3),]
-  subdat_step4 <- subdat[which(subdat$step == 4),]
-  
-  subdat_steps <- rbind(subdat_step1, subdat_step2, subdat_step3, subdat_step4)
-  
-  x <- subdat_steps$mousex_px
-  y <- subdat_steps$mousey_px
-  t <- subdat_steps$time_ms
-  
-  ab <- sqrt(diff(x)^2 + diff(y)^2) / diff(t)
-  
+library(signal)
+
+getVelocityProfile <- function(isi){
+  #plot the velocity profile of a reach within each trial
+  #we apply a butterworth filter 
+  #this corrects for the jumping data points or samples when moving
+  #we only want the lower frequencies to pass through
+  #in effect, we smooth the profile
+  par(mfrow = c(6,2), mai = c(0.3,0.3,0.3,0.3))
+  for (trialno in c(0:11)){
+    if(trialno %in% c(0:9)){
+      dat <- read.csv(sprintf('data/p000/p000-reach-b02-t0%d.csv', trialno))
+    } else{
+      dat <- read.csv(sprintf('data/p000/p000-reach-b02-t%d.csv', trialno))
+    }
+    
+    
+    #X <- df$mousex_px
+    #Y <- df$mousey_px
+    #t <- df$time_ms
+    
+    #use the splined values
+    samp <- getConstantSample(df=dat, isi=isi)
+    
+    #separate for X and Y
+    #1st order, W, only low can pass
+    #W goes from 0 to 1 (analogue vs digital, digital just converts analogue to binary)
+    #1 is the most unsmoothed value, 0 is a straight smooth line
+    #W is basically a cutoff, where lower values will pass 
+    #(note that this is not a sharp cutoff, it slopes, 
+    #so you would want the cutoff value to still be before this sloping down)
+    #W has numerator of cutoff frequency (1 Hz means moved back and forth once in a sec, 3 means more movements in a sec)
+    #denominator is Hz of signal (samples per second)
+    #if isi = 10ms, W = 3/100; isi=20, W=3/50; isi=40, W=3/25
+    #not much differences here, scale of X axis changes a bit (3.5 to 4 secs)
+    #much f differences occur at right end of plot (tails)
+    butterfilter <- butter(1, 3/120, type = 'low')
+    filtX <- filtfilt(butterfilter, samp$newX)
+    filtY <- filtfilt(butterfilter, samp$newY)
+    
+    #pythagorean theorem, essentially, to get distance
+    #diff compares value 2 minues 1, 3 minus 2, etc.
+    #distance over time
+    velocity <- (sqrt((diff(filtX))^2 + (diff(filtY))^2))/ diff(samp$newT)
+    
+    #so now plot it
+    #time will be missing one value as an effect of diff function in velocity calc
+    plot(samp$newT[2:length(samp$newT)], velocity, type = "l", col="red")
+  }
 }
 
-sqrt(diff(x)^2 + diff(y)^2) / diff(t)
+
+getConstantSample <- function(df, isi=8){
+  
+  #we take the X and Y coordinates of mouse pos
+  #however time_ms samples inconsistently
+  #so if we want coords at consistent time points
+  #we would need to interpolate coords for these times
+  #hence we do a spline
+  X <- df$mousex_px
+  Y <- df$mousey_px
+  t <- df$time_ms
+  
+  #fmm is deafult, see help file for splinefun
+  Xsplinefun <- splinefun(x=t, y = X, method = 'fmm')
+  Ysplinefun <- splinefun(x=t, y = Y, method = 'fmm')
+  
+  #isi = interstimulus interval
+  #we set it to constant samples of 20ms
+  newT <- seq(t[1],t[length(t)],by=isi)
+  
+  newX <- Xsplinefun(x=newT)
+  newY <- Ysplinefun(x=newT)
+  
+  #after spline, we get corresponding X, Y, time
+  newdf <- data.frame(newX,newY,newT)
+  
+  return(newdf)
+}
